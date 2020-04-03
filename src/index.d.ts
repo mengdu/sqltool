0288590f30b3580e7fcab7b3bfd36fa80d955428
lanyue <lanyueos@qq.com>
2020-04-03 18:40:07 +0800

    fix: 修正声明文件

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,11 @@
 // This .d.ts file is used for code prompts
 
-interface Op {
+declare type OrderString = 'DESC' | 'ASC' | 'desc' | 'asc'
+
+declare type OrderFields = { [key: string]: OrderString } | [string, OrderString][]
+
+declare type OperateType = '>' | '>=' | '<' | '<=' | '=' | '!='
+declare interface Op {
   gt (val: any): { oper: string, value: any };
   gte (val: any): { oper: string, value: any };
   lt (val: any): { oper: string, value: any };
@@ -13,52 +18,58 @@ interface Op {
   notIn (arr: any[]): { isRaw: boolean, value: string };
   like (val: string): { isRaw: boolean, value: string };
   notLike (val: string): { isRaw: boolean, value: string };
-  id (val: string, oper?: OperType): { isId: boolean, value: string };
+  id (val: string, oper?: OperateType): { isId: boolean, value: string };
   isNull (): { isRaw: boolean, value: string };
   isNotNull (): { isRaw: boolean, value: string };
   raw (sql: string): { isRaw: boolean, value: string };
 }
 
-type OrderType = 'DESC' | 'ASC' | 'desc' | 'asc'
-export type OrderFields = { [key: string]: OrderType } | [string, OrderType][]
-type OrType = { [key: string]: any }
-type AndType = OrType[]
-type OperType = '>' | '>=' | '<' | '<=' | '=' | '!='
-export type ConditionType = {
-  [key: string]: { isId?: boolean, isRaw?: boolean, oper?: OperType, value: string } | string | number | Date;
-  $or?: OrType | OrType[];
+declare interface RawValue {
+  isId?: boolean;
+  isRaw?: boolean;
+  oper?: OperateType;
+  value: string;
+}
+
+declare interface DefaultCondition {
+  [key: string]: any;
+}
+
+declare type KeyValue = RawValue | string | number | Date | DefaultCondition;
+
+declare type OrType = { [key: string]: KeyValue  }
+declare type AndType = OrType[]
+
+declare interface ConditionType extends DefaultCondition {
+  [key: string]: KeyValue;
+  $or?: OrType;
   $and?: AndType;
 }
-type OnType = [string, OperType, string] | ConditionType
-type JoinType = 'join' | 'inner join' | 'left join' | 'right join'
-export type JoinOptions = { type?: JoinType, table: string | { isRaw: boolean, value: string }, as?: string, on?: OnType }[]
-export type CreateConds = { isRaw: boolean, value: string }
+
+declare type OnType = [string, OperateType, string] | ConditionType
+declare type JoinType = 'join' | 'inner join' | 'left join' | 'right join'
+declare type JoinOptions = { type?: JoinType, table: string | { isRaw: boolean, value: string }, as?: string, on?: OnType }[]
+declare type CreateConds = { isRaw: boolean, value: string }
   | { exists: { where?: ConditionType, table?: string } }
   | { notExists: { where?: ConditionType, table?: string } }
 
 declare class Sql {
-  static alias (field: string, aliasName: string, isRaw?: boolean): string;
-  static escape (value: any): string;
-  static escapeId (value: string, isRaw?: boolean): string;
-  static raw (sql: string): { toSqlString: () => string };
-  static format (sql: string, params: object | any[]) : string;
-
-  static select (fields?: string[], table?: string): string;
-  static select (fields: { isRaw: boolean, value: string }[], table?: string): string;
-  static select (fields: [string, string][], table?: string): string;
-  static join (tables: JoinOptions): string;
-  static order (fields: OrderFields): string;
-  static group (fields: string[]): string;
-  static condition (conds: ConditionType): string[];
-  static where (conds: ConditionType): string;
-  static having (conds: ConditionType): string;
-  static create (table: string, data: { [key: string]: any }, conds?: CreateConds, ignore?: boolean): string;
-  static insert (table: string, arr: { [key: string]: any }[], ignore?: boolean): string;
-  static update (table: string, data: { [key: string]: any }, conds: ConditionType | null): string;
+  static Op:Op;
+  static alias: (field: string, aliasName: string, isRaw?: boolean) => string;
+  static escape: (value: any) => string;
+  static escapeId: (value: string, isRaw?: boolean) => string;
+  static raw: (sql: string) => { toSqlString: () => string };
+  static format: (sql: string, params: object | any[]) => string;
+  static select: (fields?: string[] | { isRaw: boolean, value: string }[] | [string, string][], table?: string) => string;
+  static join: (tables: JoinOptions) => string;
+  static order: (fields: OrderFields) => string;
+  static group: (fields: string[]) => string;
+  static condition: (conds: ConditionType) => string[];
+  static where: (conds: ConditionType) => string;
+  static having: (conds: ConditionType) => string;
+  static create: (table: string, data: { [key: string]: any }, conds?: CreateConds, ignore?: boolean) => string;
+  static insert: (table: string, arr: { [key: string]: any }[], ignore?: boolean) => string;
+  static update: (table: string, data: { [key: string]: any }, conds: ConditionType | null) => string;
 }
 
-declare namespace Sql {
-  export const Op: Op
-}
-
-export = Sql
+export default Sql
